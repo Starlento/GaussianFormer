1cfea9f5eb14f429aa86449ee380c6b4708a0ec0
Starlento <starlento@outlook.com>
2026-02-12 18:53:02 +0800

    updated pyproject

--- install_custom_ops.sh
+++ install_custom_ops.sh
@@ -17,11 +17,11 @@ ensure_pointops_init() {
 # Step 1: Install required packages
 pip install setuptools==69.5.1 packaging
 
-cd model/encoder/gaussian_encoder/ops && pip install -e . && cd -
-cd model/head/localagg && pip install -e . && cd -
+cd gaussianformer/encoder/gaussian_encoder/ops && pip install -e . && cd -
+cd gaussianformer/head/localagg && pip install -e . && cd -
 # for GaussianFormer-2
-cd model/head/localagg_prob && pip install -e . && cd -
-cd model/head/localagg_prob_fast && pip install -e . && cd -
+cd gaussianformer/head/localagg_prob && pip install -e . && cd -
+cd gaussianformer/head/localagg_prob_fast && pip install -e . && cd -
 
 # Install pointops if not already installed
 if ! pip show pointops &> /dev/null; then
